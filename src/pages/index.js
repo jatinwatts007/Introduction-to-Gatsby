295ded1d6a962a11f49bc83705bd24a0bd487afa
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-05-19 22:21:07 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Layout from '../components/layout';
-import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
-import ReadLink from '../components/read-link';
-
+import usePosts from '../hooks/use-posts';
+import Hero from '../components/hero';
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Minnesota!</p>
-      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blogs</h2>
+        {posts.map((post) => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
