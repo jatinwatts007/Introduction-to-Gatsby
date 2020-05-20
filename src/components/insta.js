56163326f739eb4f2f710e071f3fcbf673b25959
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-05-20 22:05:07 +0530

    Create insta.js

---
+++ src/components/insta.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { css } from '@emotion/core';
+import useInstagram from '../hooks/use-instagram';
+
+const Insta = () => {
+  const instaPhotos = useInstagram();
+  const username = instaPhotos[0];
+  return (
+    <>
+      <h2>Instagram posts from @{username}</h2>
+      <div>{}</div>
+      <a href={`https://instagram.com/${username}`}>
+        See more on Instagram &rarr;
+      </a>
+    </>
+  );
+};
+export default Insta;
