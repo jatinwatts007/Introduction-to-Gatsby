bf3d443e9bc796b1af8d7bb43ec767e1b9544a5d
jatinwatts007 <jatinwatts007@gmail.com>
2020-05-21 22:27:01 +0530

    Gatsby complete

---
+++ src/components/hero.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import BackgroundImage from 'gatsby-background-image';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
+const ImageBackground = styled(BackgroundImage)`
+  background-position: top 20% center;
+  background-size: cover;
+  height: 50vh;
+
+  + * {
+    margin-top: 0;
+  }
+`;
+
+const TextBox = styled('div')`
+  background-image: linear-gradient(to top, #ddbbffdd 2rem, #ddbbff00);
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  padding: 0 calc((100vw-550px) / 2) 2rem;
+  width: 100%;
+
+  h1 {
+    text-shadow: 1px 1px 3px #eeddff66;
+    font-size: 2.25rem;
+    text-align: center;
+  }
+  p,
+  a {
+    color: #222;
+    margin-top: 0;
+    text-align: center;
+  }
+
+  a {
+    margin-top: 0.5rem;
+  }
+`;
+
+const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "markus.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn="soft">
+      <TextBox>
+        <h1>Frontend Masters + Gatsby &hearts;</h1>
+        <p>
+          Hello Minnesota <Link to="/about/">Learn about me &rarr;</Link>
+        </p>
+      </TextBox>
+    </ImageBackground>
+  );
+};
+export default Hero;
